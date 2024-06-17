2d055459d43a31055b369359a643311aaaf9be3c
vinaykakkad <56934712+vinaykakkad@users.noreply.github.com>
2024-06-17 17:43:24 +0530

    Uses transformers.js and simplifies code 🤗

--- src/content.js
+++ src/content.js
@@ -2,17 +2,17 @@ const TEXT_DIV_CLASSNAME = "tfjs-mobilenet-extension-text";
 const TEXT_CONTAINER_CLASSNAME = "tfjs-mobilenet-extension-text-container";
 const HIGH_CONFIDENCE_THRESHOLD_DEFAULT = 0.5;
 const LOW_CONFIDENCE_THRESHOLD_DEFAULT = 0.1;
-const IMAGE_SIZE = 224; // Size of the image expected by mobilenet.
+const IMAGE_SIZE = 224; // Size of the image expected by the model.
 const MIN_IMG_SIZE = 128;
 
 /**
  * Produces a short text string summarizing the prediction
- * Input prediction should be a list of {className: string, prediction: float}
+ * Input prediction should be a list of {label: string, score: float}
  * objects.
- * @param {[{className: string, predictions: number}]} predictions ordered list
- *     of objects, each with a prediction class and score
+ * @param {[{label: string, score: number}]} predictions ordered list
+ *     of objects, each with a prediction label and score
  */
-async function getTextContentFromPrediction(predictions) {
+async function getTextContentFromPredictions(predictions) {
   const settings = await chrome.storage.sync.get([
     "HIGH_CONFIDENCE_THRESHOLD",
     "LOW_CONFIDENCE_THRESHOLD",
@@ -32,19 +32,19 @@ async function getTextContentFromPrediction(predictions) {
   }
 
   // Confident.
-  if (predictions[0].probability >= HIGH_CONFIDENCE_THRESHOLD) {
-    return `😄 ${predictions[0].className}!`;
+  if (predictions[0].score >= HIGH_CONFIDENCE_THRESHOLD) {
+    return `😄 ${predictions[0].label}!`;
   }
   // Not Confident.
   if (
-    predictions[0].probability >= LOW_CONFIDENCE_THRESHOLD &&
-    predictions[0].probability < HIGH_CONFIDENCE_THRESHOLD
+    predictions[0].score >= LOW_CONFIDENCE_THRESHOLD &&
+    predictions[0].score < HIGH_CONFIDENCE_THRESHOLD
   ) {
-    return `${predictions[0].className}?...\n Maybe ${predictions[1].className}?`;
+    return `${predictions[0].label}?...\n Maybe ${predictions[1].label}?`;
   }
   // Very not confident.
-  if (predictions[0].probability < LOW_CONFIDENCE_THRESHOLD) {
-    return `😕  ${predictions[0].className}????...\n Maybe ${predictions[1].className}????`;
+  if (predictions[0].score < LOW_CONFIDENCE_THRESHOLD) {
+    return `😕  ${predictions[0].label}????...\n Maybe ${predictions[1].label}????`;
   }
 }
 
@@ -70,11 +70,6 @@ function addTextElementToImageNode(imgNode, textContent) {
   text.textContent = textContent;
 }
 
-// Add a listener to hear from the content.js page when the image is through
-// processing.  The message should contain an action, a url, and predictions (the
-// output of the classifier)
-//
-// message: {action, url, predictions}
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (!message) {
     return;
@@ -90,26 +85,18 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       Array.from(document.querySelectorAll('img[class="YQ4gaf"]'))
         .slice(0, 10)
         .forEach((image) => {
-          getImageDataFromSrc(image.src).then((imageData) =>
-            chrome.runtime.sendMessage({
-              action: "ANALYZE_IMAGE",
-              imageData: imageData,
-              src: image.src,
-            })
-          );
+          chrome.runtime.sendMessage({
+            action: "ANALYZE_IMAGE",
+            src: image.src,
+          });
         });
       break;
-    case "GET_IMAGE_DATA":
-      getImageDataFromSrc(message.url).then((imageData) =>
-        sendResponse(imageData)
-      );
-      return true;
     case "ADD_PREDICTION_TO_IMAGES":
       if (message.url && message.predictions) {
         Array.from(document.getElementsByTagName("img"))
           .filter((x) => x.src === message.url)
           .forEach((node) => {
-            getTextContentFromPrediction(message.predictions).then(
+            getTextContentFromPredictions(message.predictions).then(
               (textContent) => addTextElementToImageNode(node, textContent)
             );
           });
@@ -119,42 +106,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       break;
   }
 });
-
-function getImageDataFromSrc(src) {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.crossOrigin = "anonymous";
-    img.onerror = function (e) {
-      console.warn(`Could not load image from external source ${src}.`);
-      resolve(null);
-    };
-    img.onload = function (e) {
-      // When image is loaded, render it to a canvas and send its ImageData back
-      // to the service worker.
-      if (
-        (img.height && img.height > MIN_IMG_SIZE) ||
-        (img.width && img.width > MIN_IMG_SIZE)
-      ) {
-        img.width = IMAGE_SIZE;
-        img.height = IMAGE_SIZE;
-
-        const canvas = new OffscreenCanvas(img.width, img.height);
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
-
-        const imageData = ctx.getImageData(0, 0, img.width, img.height);
-        resolve({
-          data: Array.from(imageData.data),
-          height: imageData.height,
-          width: imageData.width,
-        });
-        return;
-      }
-      console.warn(
-        `Image size too small. [${img.height} x ${img.width}] vs. minimum [${MIN_IMG_SIZE} x ${MIN_IMG_SIZE}]`
-      );
-      resolve(null);
-    };
-    img.src = src;
-  });
-}
--- src/service_worker.js
+++ src/service_worker.js
@@ -1,120 +1,63 @@
-import { load } from "@tensorflow-models/mobilenet";
-import { tidy, zeros } from "@tensorflow/tfjs";
+import { pipeline, env } from "@xenova/transformers";
 
-const IMAGE_SIZE = 224;
 const TOP_K_PREDICTIONS = 2;
-const FIVE_SECONDS_IN_MS = 5000;
+env.backends.onnx.wasm.numThreads = 1;
 
 /**
- * What action to take when someone clicks the right-click menu option.
- * Here it takes the url of the right-clicked image and the current tabId, and
- * send them to the content script where the ImageData will be retrieved and
- * sent back here. After that, imageClassifier's analyzeImage method.
+ * A singelton class the manages the image classification pipeline. Ensures that only
+ * one instance of the pipeline is created and is reused throughout the application.
  */
-function clickMenuCallback(info, tab) {
-  const message = { action: "GET_IMAGE_DATA", url: info.srcUrl };
-  chrome.tabs.sendMessage(tab.id, message, (resp) => {
-    if (resp === null) {
-      console.error(
-        "Failed to get image data. " +
-          "The image might be too small or failed to load. " +
-          "See console logs for errors."
-      );
-      return;
-    }
-    const image = new ImageData(
-      new Uint8ClampedArray(resp.data),
-      resp.width,
-      resp.height
-    );
-    imageClassifier.analyzeImageAndSendMessage(image, info.srcUrl, tab.id);
-  });
-}
-
-/**
- * Async loads a mobilenet on construction.  Subsequently handles
- * requests to classify images through the .analyzeImage API.
- * Successful requests will post a chrome message with
- * 'ADD_PREDICTION_TO_IMAGES' action, which the content.js can
- * hear and use to manipulate the DOM.
- */
-class ImageClassifier {
-  constructor() {
-    this.loadModel();
-  }
+class ClassificationPipelineSingleton {
+  static task = "image-classification";
+  static model = "Xenova/swin-base-patch4-window7-224-in22k";
+  static instance = null;
 
   /**
-   * Loads mobilenet from URL and keeps a ref erence to it in the object.
-   */
-  async loadModel() {
-    console.log("Loading model...");
-    const startTime = performance.now();
-    try {
-      this.model = await load({ version: 2, alpha: 1.0 });
-      // Warms up the model by causing intermediate tensor values
-      // to be built and pushed to GPU.
-      tidy(() => {
-        this.model.classify(zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3]));
-      });
-      const totalTime = Math.floor(performance.now() - startTime);
-      console.log(`Model loaded and initialized in ${totalTime} ms...`);
-    } catch (e) {
-      console.error("Unable to load model", e);
-    }
-  }
-
-  /**
-   * Triggers the model to make a prediction on the image referenced by the
-   * image data. After a successful prediction a IMAGE_CLICK_PROCESSED message
-   * when complete, for the content.js script to hear and update the DOM with
-   * the results of the prediction.
+   * Asynchronously retrieves the singleton instance of the classification pipeline.
+   * If the instance doesn't exist, it creates a new one using the task and model.
    *
-   * @param {ImageData} imageData ImageData of the image to analyze.
-   * @param {string} url url of image to analyze.
-   * @param {number} tabId which tab the request comes from.
+   * @param {Function} [progress_callback=null] - An optional callback function to track
+   * the progress of the pipeline creation.
+   * @returns {Promise<Object>} A promise that resolves to the singleton instance of the
+   * classification pipeline.
    */
-  async analyzeImageAndSendMessage(imageData, url, tabId) {
-    if (!this.model) {
-      console.log("Waiting for model to load...");
-      setTimeout(
-        () => this.analyzeImageAndSendMessage(imageData, url, tabId),
-        FIVE_SECONDS_IN_MS
-      );
-      return;
+  static async getInstance(progress_callback = null) {
+    if (this.instance === null) {
+      this.instance = pipeline(this.task, this.model, { progress_callback });
     }
-    console.log("Predicting...");
-    const startTime = performance.now();
-    const predictions = await this.model.classify(imageData, TOP_K_PREDICTIONS);
-    const totalTime = performance.now() - startTime;
-    console.log(`Done in ${totalTime.toFixed(1)} ms `);
-    const message = {
-      action: "ADD_PREDICTION_TO_IMAGES",
-      url,
-      predictions,
-    };
-    chrome.tabs.sendMessage(tabId, message);
+
+    return this.instance;
   }
 }
 
-const imageClassifier = new ImageClassifier();
+/**
+ * Asynchronously classifies the given text using the classification pipeline.
+ * @param {string} url - URL of the image to be classified.
+ * @param {number} tabId - id of the tab with the image.
+ * @returns {Promise<Object>} A promise that resolves to the classification result.
+ */
+async function analyzeImageAndSendMessage(url, tabId) {
+  console.log("Predicting...");
+  const startTime = performance.now();
+  let model = await ClassificationPipelineSingleton.getInstance();
+  const predictions = await model(url, { topk: TOP_K_PREDICTIONS });
+  const totalTime = performance.now() - startTime;
+  console.log(`Done in ${totalTime.toFixed(1)} ms `);
+  const message = {
+    action: "ADD_PREDICTION_TO_IMAGES",
+    url,
+    predictions,
+  };
+  chrome.tabs.sendMessage(tabId, message);
+}
 
 /**
  * Adds a listener to listen for messages from the content script for analyzing images.
  */
-
 chrome.runtime.onMessage.addListener(
   async function (message, sender, sendResponse) {
     if (message.action === "ANALYZE_IMAGE") {
-      const image = new ImageData(
-        new Uint8ClampedArray(message.imageData.data),
-        message.imageData.width,
-        message.imageData.height
-      );
-      imageClassifier.analyzeImageAndSendMessage(
-        image,
-        message.src,
-        sender.tab.id
-      );
+      analyzeImageAndSendMessage(message.src, sender.tab.id);
     }
   }
 );
@@ -126,9 +69,11 @@ chrome.runtime.onMessage.addListener(
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "contextMenu0",
-    title: "Classify image with TensorFlow.js ",
+    title: "Classify image with Transformers.js ",
     contexts: ["image"],
   });
 });
 
-chrome.contextMenus.onClicked.addListener(clickMenuCallback);
+chrome.contextMenus.onClicked.addListener((info, tab) =>
+  analyzeImageAndSendMessage(info.srcUrl, tab.id)
+);
